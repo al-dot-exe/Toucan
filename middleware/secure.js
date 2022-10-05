b33a6f38921d117f6e66a6ceec198d902be74400
Al.exe <alEXE-tech@protonmail.com>
2022-10-05 13:01:24 -0700

    Fixed for now: however http and https protocols need to be given necessary permissions and port numbers on your own instance

--- middleware/secure.js
+++ middleware/secure.js
@@ -1,14 +1,13 @@
-require("dotenv").config({ path: "config/.env" }); // .env config route
-//
+require("dotenv").config({ path: "../config/.env" }); // .env config route
+
 module.exports = {
   ensureSecure: (req, res, next) => {
+
     const HTTPS = process.env.PORT_HTTPS || 5000;
-    if (process.env.NODE_ENV === "production") {
-      if (req.headers["x-forwarded-proto"] !== "https") {
-        return res.redirect("https://" + req.headers.host + req.url + `:${HTTPS}`);
-      } else {
-        return next();
-      }
+    console.log(req.protocol);
+    console.log(req.hostname)
+      if (req.protocol !== "https" || !req.protocol) {
+        return res.redirect("https://" + req.hostname + `:${HTTPS}` + req.url);
     } else {
       return next();
     }
