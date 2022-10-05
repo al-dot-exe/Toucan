7c0eab1d5f299e853df8ba3ccb510792af166f42
Al.exe <alEXE-tech@protonmail.com>
2022-10-05 13:06:04 -0700

    ...

--- middleware/secure.js
+++ middleware/secure.js
@@ -1,11 +1,8 @@
 require("dotenv").config({ path: "../config/.env" }); // .env config route
-
 module.exports = {
   ensureSecure: (req, res, next) => {
 
     const HTTPS = process.env.PORT_HTTPS || 5000;
-    console.log(req.protocol);
-    console.log(req.hostname)
       if (req.protocol !== "https" || !req.protocol) {
         return res.redirect("https://" + req.hostname + `:${HTTPS}` + req.url);
     } else {
