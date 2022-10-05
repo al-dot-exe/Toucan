e624943b6ecbae8beab84228feacb79f6e5d958d
Al.exe <alEXE-tech@protonmail.com>
2022-10-05 12:30:44 -0700

    Fixed redirection issue

--- middleware/secure.js
+++ middleware/secure.js
@@ -1,9 +1,11 @@
+require("dotenv").config({ path: "config/.env" }); // .env config route
+//
 module.exports = {
   ensureSecure: (req, res, next) => {
-    console.log(req);
+    const HTTPS = process.env.PORT_HTTPS || 5000;
     if (process.env.NODE_ENV === "production") {
       if (req.headers["x-forwarded-proto"] !== "https") {
-        return res.redirect("https://" + req.headers.host + req.url);
+        return res.redirect("https://" + req.headers.host + req.url + `:${HTTPS}`);
       } else {
         return next();
       }
